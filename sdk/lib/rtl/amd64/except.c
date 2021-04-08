202331bf118a7dc3d4156149aa1122f5b529dc0c
Victor Perevertkin <victor.perevertkin@reactos.org>
2021-04-08 19:06:48 +0300

    [RTL] Implement exception and unwind code for amd64
    Messages from Timo:
    
    [RTL] Implement RtplUnwindInternal and wrap RtlUnwindEx and RtlDispatchException around it
    
    Based on the great description in this blog article: http://www.nynaeve.net/?p=106
    A few things are done differently
    
    * Add support for version 2 unwind info
    * Implement UnwindOpSlots() and use it (based on https://github.com/dotnet/coreclr/blob/master/src/unwinder/amd64/unwinder_amd64.cpp)
    * Fix handling of UWOP_PUSH_MACHFRAME
    
    [RTL] Implement RtlCaptureNonVolatileContextPointers and RtlSetUnwindContext
    
    Instead of only checking for the TargetFrame, also check for a mode change, i.e. RIP went from kernel to user,
    in which case the target frame was not reached yet, because it was too large, but processing can't continue here.
    
    [RTL] Use movaps instead of movdqa in RtlCaptureContext
    
    It does the same thing, but is one byte shorter
    
    Co-authored-by: Timo Kreuzer <timo.kreuzer@reactos.org>

---
+++ sdk/lib/rtl/amd64/except.c
@@ -0,0 +1,129 @@
+/*
+* COPYRIGHT:         See COPYING in the top level directory
+* PROJECT:           ReactOS Run-Time Library
+* PURPOSE:           User-mode exception support for AMD64
+* FILE:              lib/rtl/amd64/except.c
+* PROGRAMERS:        Timo Kreuzer (timo.kreuzer@reactos.org)
+*/
+
+/* INCLUDES *****************************************************************/
+
+#include <rtl.h>
+#define NDEBUG
+#include <debug.h>
+
+/* PUBLIC FUNCTIONS **********************************************************/
+
+#if 0
+VOID
+NTAPI
+RtlRaiseException(IN PEXCEPTION_RECORD ExceptionRecord)
+{
+    CONTEXT Context;
+    NTSTATUS Status = STATUS_INVALID_DISPOSITION;
+    ULONG64 ImageBase;
+    PRUNTIME_FUNCTION FunctionEntry;
+    PVOID HandlerData;
+    ULONG64 EstablisherFrame;
+
+    /* Capture the context */
+    RtlCaptureContext(&Context);
+
+    /* Get the function entry for this function */
+    FunctionEntry = RtlLookupFunctionEntry(Context.Rip,
+                                           &ImageBase,
+                                           NULL);
+
+    /* Check if we found it */
+    if (FunctionEntry)
+    {
+        /* Unwind to the caller of this function */
+        RtlVirtualUnwind(UNW_FLAG_NHANDLER,
+                         ImageBase,
+                         Context.Rip,
+                         FunctionEntry,
+                         &Context,
+                         &HandlerData,
+                         &EstablisherFrame,
+                         NULL);
+
+        /* Save the exception address */
+        ExceptionRecord->ExceptionAddress = (PVOID)Context.Rip;
+
+        /* Check if user mode debugger is active */
+        if (RtlpCheckForActiveDebugger())
+        {
+            /* Raise an exception immediately */
+            Status = ZwRaiseException(ExceptionRecord, &Context, TRUE);
+        }
+        else
+        {
+            /* Dispatch the exception and check if we should continue */
+            if (!RtlDispatchException(ExceptionRecord, &Context))
+            {
+                /* Raise the exception */
+                Status = ZwRaiseException(ExceptionRecord, &Context, FALSE);
+            }
+            else
+            {
+                /* Continue, go back to previous context */
+                Status = ZwContinue(&Context, FALSE);
+            }
+        }
+    }
+
+    /* If we returned, raise a status */
+    RtlRaiseStatus(Status);
+}
+#endif
+
+/*
+* @unimplemented
+*/
+PVOID
+NTAPI
+RtlpGetExceptionAddress(VOID)
+{
+    UNIMPLEMENTED;
+    return NULL;
+}
+
+BOOLEAN
+NTAPI
+RtplUnwindInternal(
+    _In_opt_ PVOID TargetFrame,
+    _In_opt_ PVOID TargetIp,
+    _In_ PEXCEPTION_RECORD ExceptionRecord,
+    _In_ PVOID ReturnValue,
+    _In_ PCONTEXT ContextRecord,
+    _In_opt_ struct _UNWIND_HISTORY_TABLE *HistoryTable,
+    _In_ ULONG Flags);
+
+/*
+ * @unimplemented
+ */
+BOOLEAN
+NTAPI
+RtlDispatchException(
+    _In_ PEXCEPTION_RECORD ExceptionRecord,
+    _In_ PCONTEXT ContextRecord)
+{
+    /* Perform vectored exception handling for user mode */
+    if (RtlCallVectoredExceptionHandlers(ExceptionRecord, ContextRecord))
+    {
+        /* Exception handled, now call vectored continue handlers */
+        RtlCallVectoredContinueHandlers(ExceptionRecord, ContextRecord);
+
+        /* Continue execution */
+        return TRUE;
+    }
+
+    /* Call the internal unwind routine */
+    return RtplUnwindInternal(NULL, // TargetFrame
+                              NULL, // TargetIp
+                              ExceptionRecord,
+                              0, // ReturnValue
+                              ContextRecord,
+                              NULL, // HistoryTable
+                              UNW_FLAG_EHANDLER);
+}
